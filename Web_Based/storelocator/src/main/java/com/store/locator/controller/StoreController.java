ff2b35b9ca1d241d9047db7f3faa5fa505be6c65
Vikhyat <vikhyat.kaushik@gmail.com>
2016-06-25 18:55:23 +0530

    Checking in Create, Update and Delete functionality for Stores

---
+++ Web_Based/storelocator/src/main/java/com/store/locator/controller/StoreController.java
@@ -0,0 +1,93 @@
+package com.store.locator.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.store.locator.model.Store;
+import com.store.locator.service.StoreServiceImpl;
+
+@RestController
+public class StoreController {
+
+	@Autowired
+	StoreServiceImpl storeService; 
+
+	/**
+	 * Creates a new store
+	 * @param store
+	 * @param ucBuilder
+	 * @return
+	 */
+	@RequestMapping(value = "/store/", method = RequestMethod.POST)
+	public ResponseEntity<Void> createStore(@RequestBody Store store, UriComponentsBuilder ucBuilder) {
+		System.out.println("Creating Store " + store.getName());
+		if (storeService.isStoreExist(store)) {
+			System.out.println("A Store with name " + store.getName() + " already exist");
+			return new ResponseEntity<Void>(HttpStatus.CONFLICT);
+		}
+		storeService.saveStore(store);
+		return new ResponseEntity<Void>(HttpStatus.CREATED);
+	}
+
+	/**
+	 * Updates an existing store
+	 * @param id
+	 * @param store
+	 * @return
+	 */
+	@RequestMapping(value = "/store/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Store> updateStore(@PathVariable("id") long id, @RequestBody Store store) {
+		System.out.println("Updating Store " + id);
+		Store currentStore = storeService.findById(id);
+		if (currentStore == null) {
+			System.out.println("Store with id " + id + " not found");
+			return new ResponseEntity<Store>(HttpStatus.NOT_FOUND);
+		}
+		currentStore.setName(store.getName());
+		currentStore.setAddress(store.getAddress());
+		currentStore.setLatitude(store.getLatitude());
+		currentStore.setLongitude(store.getLongitude());
+		storeService.updateStore(currentStore);
+		return new ResponseEntity<Store>(currentStore, HttpStatus.OK);
+	}
+
+	/**
+	 * Deletes an existing store
+	 * @param id
+	 * @return
+	 */
+	@RequestMapping(value = "/store/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Store> deleteStore(@PathVariable("id") long id) {
+		System.out.println("Fetching & Deleting Store with id " + id);
+		Store store = storeService.findById(id);
+		if (store == null) {
+			System.out.println("Unable to delete. Store with id " + id + " not found");
+			return new ResponseEntity<Store>(HttpStatus.NOT_FOUND);
+		}
+		storeService.deleteStore(id);
+		return new ResponseEntity<Store>(HttpStatus.NO_CONTENT);
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	@RequestMapping(value = "/store", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Store>> deleteAllStores(@RequestParam("zipcode") String zipcode, @RequestParam("radius") long radius) {
+		System.out.println("Finding All Stores in vicinity");
+		List<Store> storesList = storeService.findStoresByVicinity(zipcode, radius);
+		return new ResponseEntity<List<Store>>(storesList, HttpStatus.NO_CONTENT);
+	}
+
+}
---
+++ Web_Based/storelocator/src/main/java/com/store/locator/model/Store.java
@@ -0,0 +1,114 @@
+package com.store.locator.model;
+
+public class Store {
+
+	private long id;
+	private String name;
+	private String address;
+	private String latitude;
+	private String longitude;
+
+	public Store() {
+		id = 0;
+	}
+
+	public Store(long id, String name, String address, String latitude, String longitude) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+
+	public String getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((address == null) ? 0 : address.hashCode());
+		result = prime * result + ((latitude == null) ? 0 : latitude.hashCode());
+		result = prime * result + ((longitude == null) ? 0 : longitude.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Store other = (Store) obj;
+		if (address == null) {
+			if (other.address != null)
+				return false;
+		} else if (!address.equals(other.address))
+			return false;
+		if (latitude == null) {
+			if (other.latitude != null)
+				return false;
+		} else if (!latitude.equals(other.latitude))
+			return false;
+		if (longitude == null) {
+			if (other.longitude != null)
+				return false;
+		} else if (!longitude.equals(other.longitude))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Store [id=" + id + ", name=" + name + ", address=" + address + ", latitude=" + latitude + ", longitude="
+				+ longitude + "]";
+	}
+
+}
